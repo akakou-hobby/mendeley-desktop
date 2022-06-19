fe2e480c66c0e64a562c27c650fa5b82868e04f0
akakou <akakou571@gmail.com>
2022-06-19 15:19:15 +0900

    [add] sandbox

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    sandbox: true
   })
 
   win.loadURL('https://www.mendeley.com/reference-manager/library/all-references')
